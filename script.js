ab60a52065c8d2b234ae22feed2e798e78982cad
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-08-19 09:48:16 -0400

    created random button and added more css

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color-1");
 var color2 = document.querySelector(".color-2");
 var body = document.getElementById("gradient");
+var randomGrad = document.getElementById("random-gradient");
 
 function initialColors() {
   color1.value = "#FF0000";
@@ -16,8 +17,25 @@ function setGradient() {
   css.textContent = body.style.background + ";";
 }
 
+function randomColors() {
+  let randomColor1 =  Math.floor(Math.random()*16777215).toString(16);
+  let randomColor2 =  Math.floor(Math.random()*16777215).toString(16);
+  color1.value = "#" + randomColor1;
+  color2.value = "#" + randomColor2;
+
+  setGradient();
+}
+
+function colorAnimation() {
+  
+}
+
 color1.addEventListener("input", setGradient)
 
 color2.addEventListener("input", setGradient)
+
+randomGrad.addEventListener("click", randomColors)
+
+
 
 initialColors();
